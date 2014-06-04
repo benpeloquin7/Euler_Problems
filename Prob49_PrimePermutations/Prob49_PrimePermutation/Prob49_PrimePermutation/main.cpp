fef8984e8b19097dd692cd8670432e4b5a0604c8
benpeloquin7 <benjamin.peloquin@gmail.com>
2014-06-03 18:51:24 -0700

    Initial Commit

---
+++ Prob49_PrimePermutations/Prob49_PrimePermutation/Prob49_PrimePermutation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Prob49_PrimePermutation
+//
+//  Created by Ben Peloquin on 6/3/14.
+//  Copyright (c) 2014 B. Peloquin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
